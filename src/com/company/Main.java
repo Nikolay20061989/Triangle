e909aa08b01bf9870b1e6ae966f17e17c44f8f87
Admin <ivashenko.n77@gmail.com>
2020-11-16 22:00:00 +0200

    коментарий

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	Triangle t1= new Triangle(14,12,12);
-	t1.geroneSquare();
+        System.out.println (t1.geroneSquare());
 	Triangle t2=new Triangle(8,5,6);
-	t2.geroneSquare();
+        System.out.println (t2.geroneSquare());
+
 
 
     }
--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -6,6 +6,7 @@ public class Triangle {
     public double side3;
 
 
+
     public Triangle(double side1, double side2, double side3) {
         this.side1 = side1;
         this.side2 = side2;
@@ -13,9 +14,34 @@ public class Triangle {
 
     }
 
-    public  void geroneSquare() {
+    public double geroneSquare() {
         double p = (side1 + side2 + side3) / 2;
-        System.out.println(Math.sqrt(p * (p - side1) * (p - side2) * (p - side3)));
+        double a=Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
+        return  a;
+    }
+public Triangle(){}
+
+    public double getSide1() {
+        return side1;
     }
 
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
 }
